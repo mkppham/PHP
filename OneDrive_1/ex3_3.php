171cc30bf6217f03dfa1725ed166c91a06df36cc
pham kim kien <142959527+mkppham@users.noreply.github.com>
2024-01-10 09:49:37 +0700

    Add files via upload

---
+++ OneDrive_1/ex3_3.php
@@ -0,0 +1,7 @@
+<?php
+    $mycounter = 1;
+    $mystring = "hello";
+    $myarray = array("One", "Two", "Three");
+
+    echo $mycounter;
+?>
---
+++ OneDrive_1/ex3_9.php
@@ -0,0 +1,12 @@
+<?php
+    $author = "Scott Adams";
+
+    $out = <<<_SEC 
+    Nomal people believe that if it ain't broke, don't fix it.
+    Engineers believe that if it ain't broke, it doesn't have enough
+    features yet.
+
+    - $author.
+-SEC;
+    echo $out;
+ ?>
