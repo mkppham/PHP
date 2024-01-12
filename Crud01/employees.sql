c7d38b1bd5ae78c4ba4de8044fc91e3785dce018
pham kim kien <142959527+mkppham@users.noreply.github.com>
2024-01-12 09:49:37 +0700

    students.sql

--- Crud01/employees.sql
+++ Crud01/employees.sql
@@ -1,6 +1,11 @@
-c
-
-INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
-(1, 'Roland Mendel', 'C/ Araquil, 67, Madrid', 5000),
-(2, 'Victoria Ashworth', ’35 King George, London’, 6500),
-(3, ’Martin Blank’, ’25, Rue Lauriston, Paris’, 8000);
+CREATE TABLE if not exists `students`(
+                                         id int(11) NOT NULL AUTO_INCREMENT,
+    name Nvarchar(100) NOT NULL ,
+    email varchar(250) NOT NULL ,
+    PRIMARY KEY (id)
+    ) ENGINE =InnoDB DEFAULT  CHARSET = latin1 AUTO_INCREMENT=2;
+INSERT INTO `students` (`id`, `name`, `email`) VALUES
+                                                   (1, 'Pham Van A', 'PAV@gmail.com'),
+                                                   (2, 'Nguyen Trong H', 'NTH@gmail.com'),
+                                                   (3, 'Mai Viet D', 'MVD@gmail.com'),
+                                                   (4, 'Pham Kim K', 'MKP@gmail.com');
