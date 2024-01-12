dffed6d03c7bc6dc07d6539d8427ae57494cea12
pham kim kien <142959527+mkppham@users.noreply.github.com>
2024-01-12 09:51:06 +0700

    read.php

--- Crud01/read.php
+++ Crud01/read.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 // Check existence of id parameter before processing further
 if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     // Include config file
     require_once 'config.php';
-    
+
     // Prepare a select statement
     $sql = "SELECT * FROM employees WHERE id = ?";
-    
+
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt, "i", $param_id);
-        
+
         // Set parameters
         $param_id = trim($_GET["id"]);
-        
+
         // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
             $result = mysqli_stmt_get_result($stmt);
-            
+
             if(mysqli_num_rows($result) == 1){
                 /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
                 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                
+
                 // Retrieve individual field value
                 $name = $row["name"];
                 $address = $row["address"];
@@ -31,7 +31,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                 header("location: error.php");
                 exit();
             }
-            
+
         } else{
             echo "Oops! Something went wrong. Please try again later.";
         }
@@ -40,7 +40,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     mysqli_stmt_close($stmt);
 // Close connection
     mysqli_close($link);
-    } else{
+} else{
 // URL doesn't contain id parameter. Redirect to error page
     header("location: error.php");
     exit();
@@ -49,17 +49,17 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>View Record</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-        <style type="text/css">
-            .wrapper{
-                width: 500px;
-                margin: 0 auto;
-            }
-        </style>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
 <body>
 <div class="wrapper">
     <div class="container-fluid">
@@ -74,14 +74,9 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                     <label>Address</label>
                     <p class="form-control-static"><?php echo $row["address"]; ?></p>
                 </div>
-                <div class="form-group">
-                    <label>Salary</label>
-                    <p class="form-control-static"><?php echo $row["salary"]; ?></p>
-                </div>
                 <p><a href="index.php" class="btn btn-primary">Back</a></p>
             </div>
-        </div>    
+        </div>
     </div>
 </body>
 </html>
-
