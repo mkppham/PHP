5e982912dd3daf512fb26c7ab875303169d39a8d
pham kim kien <142959527+mkppham@users.noreply.github.com>
2024-01-12 09:47:53 +0700

    create.php

--- Crud01/create.php
+++ Crud01/create.php
@@ -1,77 +1,53 @@
 <?php
-// Include config file
-require_once "config.php";
+global $link;
+require_once 'config.php';
 
-// Define variables and initialize with empty values
-$name_err = $address_err = $salary_err = "";
-$name = $address = $salary = "";
+$name = $email = "";
+$name_err = $email_err = "";
 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    // Validate name
     $input_name = trim($_POST["name"]);
-    if(empty($input_name)){
-        $name_err = "Please enter a name.";
-    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z-' ]*$/")))){
-        $name_err = "Please enter a valid name.";
-    } else{
-        $name = $input_name;
-    }
-
-    // Validate address
-    $input_address = trim($_POST["address"]);
-    if(empty($input_address)){
-        $address_err = "Please enter an address.";
-    } else{
-        $address = $input_address;
-    }
-
-    // Validate salary
-    $input_salary = trim($_POST["salary"]);
-    if(empty($input_salary)){
-        $salary_err = "Please enter the salary amount.";
-    }elseif(!ctype_digit($input_salary)){
-        $salary_err = 'Please enter a positive integer value.';
+    if (empty($input_name)){
+        $name_err = "Please enter a name";
+    } elseif(!filter_var(trim($_POST["name"]), FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/^[a-zA-Z' -.\s]+$/")))){
+        $name_err = 'Invalid name';
     }else{
-        $salary = $input_salary;
+        $name= $input_name;
     }
 
-    // Check input errors before inserting in database
-    if(empty($name_err) && empty($address_err) && empty($salary_err)){
-    // Prepare an insert statement
-    $sql = "INSERT INTO employees (name, address, salary) VALUES (?, ?, ?)";
-    
-    if($stmt = mysqli_prepare($link, $sql)){
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_address, $param_salary);
-        
-        // Set parameters
-        $param_name = $name;
-        $param_address = $address;
-        $param_salary = $salary;
-        
-        // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt)){
-            // Records created successfully. Redirect to landing page
-            header("Location: index.php");
-            exit();
-        } else{
-            echo "Something went wrong. Please try again later.";
+    $input_email = trim($_POST["email"]);
+    if(empty($input_email)){
+        $email_err = 'Please enter an Email.';
+    } else{
+        $email = $input_email;
+    }
+    if (empty($name_err) && empty($email_err)){
+        $sql = "INSERT INTO `students` (name, email) VALUES (?,?)";
+
+        if ($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "ss", $param_name, $param_email);
+
+            $param_name = $name;
+            $param_email = $email;
+
+            if (mysqli_stmt_execute($stmt)){
+                header("location:index.php");
+                exit();
+            } else{
+                echo "Something went WRONG";
+            }
         }
         mysqli_stmt_close($stmt);
     }
     mysqli_close($link);
 }
-}
 ?>
-
-
-<!DOCTYPE html>
+<!DOCTYPE  html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Create Record</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
@@ -79,38 +55,38 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             margin: 0 auto;
         }
     </style>
+
 </head>
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h2>Create Record</h2>
-                    </div>
-                    <p>Please fill this form and submit to add employee record to the database.</p>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
-                            <label>Name</label>
-                            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
-                            <span class="help-block"><?php echo $name_err;?></span>
-                        </div>
-                        <div class="form-group <?php echo (!empty($address_err)) ? 'has-error' : ''; ?>">
-                            <label>Address</label>
-                            <textarea name="address" class="form-control"><?php echo $address; ?></textarea>
-                            <span class="help-block"><?php echo $address_err;?></span>
-                        </div>
-                        <div class="form-group <?php echo (!empty($salary_err)) ? 'has-error' : ''; ?>">
-                            <label>Salary</label>
-                            <input type="text" name="salary" class="form-control" value="<?php echo $salary; ?>">
-                            <span class="help-block"><?php echo $salary_err;?></span>
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="index.php" class="btn btn-default">Cancel</a>
-                    </form>
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h2>Create Record</h2>
                 </div>
-            </div> 
+                <p>Fill this form and submit to add student record to the database</p>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                    <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                        <label>Name</label>
+                        <label>
+                            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
+                        </label>
+                        <span class="help-block"><?php echo $name_err?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($email_err))? 'has-error' : '';?>">
+                        <label>Email</label>
+                        <label>
+                            <textarea name="email" class="form-control"><?php echo $email;?></textarea>
+                        </label>
+                        <span class="help-block"><?php echo $email_err;?></span>
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                    <a href="index.php" class="btn btn-default">Cancel</a>
+                </form>
+            </div>
         </div>
     </div>
+</div>
 </body>
-</html>                   
+</html>
