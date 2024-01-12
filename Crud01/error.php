0e5e8ba877a55c1f6adc3bc1439b357e270c4cfe
pham kim kien <142959527+mkppham@users.noreply.github.com>
2024-01-12 09:50:01 +0700

    error.php

--- Crud01/error.php
+++ Crud01/error.php
@@ -12,20 +12,20 @@
     </style>
 </head>
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h1>Invalid Request</h1>
-                    </div>
-                    <div class="alert alert-danger fade in">
-                        <p>Sorry, you've made an invalid request. Please 
-                        <a href="index.php" class="alert-link">go back</a> 
-                        and try again.</p>
-                    </div>
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1>Invalid Request</h1>
                 </div>
-            </div>        
+                <div class="alert alert-danger fade in">
+                    <p>Sorry, you've made an invalid request. Please
+                        <a href="index.php" class="alert-link">go back</a>
+                        and try again.</p>
+                </div>
+            </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
